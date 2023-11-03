022756006b9c66884034a512d97d4b9be2969603
TWAGIRAYESU MAHIRWE GIFT VIVIEN <149520489+GiftVivien@users.noreply.github.com>
2023-11-03 22:39:19 +0200

    Update conditionExample.java

--- src/conditionExample.java
+++ src/conditionExample.java
@@ -1,14 +1,14 @@
-public class conditionExample {
-    public static void main(String [] args){
-        int a=10;
-        int b=15;
+public class conditionExample {// class name 
+    public static void main(String [] args){// mandatory main method
+        int a=10;// initialize value
+        int b=15;// initialize method
         
         if (a>b) {
-            System.out.println("a is greater than b");
+            System.out.println("a is greater than b");// first output
             
         } else {
             
-            System.out.println("b is greater than a");
+            System.out.println("b is greater than a");//alternative output
             
         }
          
