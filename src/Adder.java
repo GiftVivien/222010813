7966d591e94ec8c6430a883ef26369885a5c4c0a
TWAGIRAYESU MAHIRWE GIFT VIVIEN <149520489+GiftVivien@users.noreply.github.com>
2023-11-03 22:04:00 +0200

    Update Adder.java

--- src/Adder.java
+++ src/Adder.java
@@ -1,13 +1,13 @@
-public class Adder {
-    public int add(int num1, int num2){
-        return num1 + num2;
+public class Adder {//class name
+    public int add(int num1, int num2){//class method
+        return num1 + num2;// return value
 
     }
     public class Main{
-        public static void main(String[] args) {
-            Adder myAdder= new Adder();
-            int result= myAdder.add(5,3);
-            System.out.println("result: " + result);
+        public static void main(String[] args) {// mandatory main method
+            Adder myAdder= new Adder();// object name
+            int result= myAdder.add(5,3);//recalling class method
+            System.out.println("result: " + result);// output message
             
         }
     }
