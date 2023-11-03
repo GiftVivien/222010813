41ee560819d5fa8c358e57733b3248658b65c711
TWAGIRAYESU MAHIRWE GIFT VIVIEN <149520489+GiftVivien@users.noreply.github.com>
2023-11-03 22:34:38 +0200

    Update compoundInterest.java

--- src/compoundInterest.java
+++ src/compoundInterest.java
@@ -1,19 +1,19 @@
-public class compoundInterest {
+public class compoundInterest {// class name 
     
-    public static double Interest (double deposit,double rate, double time ){
+    public static double Interest (double deposit,double rate, double time ){// class method
         
-        return deposit*Math.pow(1+rate/100/12, time*12);
+        return deposit*Math.pow(1+rate/100/12, time*12);// return value
 
 
     }
     
 
-    public static void main(String[] args) {
-     double depo= 500000;
-     double percentage= 18;
-     double timePeriod= 3;
+    public static void main(String[] args) {//mandatory main method
+     double depo= 500000;// initialize value
+     double percentage= 18;// initialize value
+     double timePeriod= 3;// initialize value
      double compoundInterest = Interest(depo, percentage, timePeriod);
-     System.out.println("The compound interest of the deposited amount: " + compoundInterest);
+     System.out.println("The compound interest of the deposited amount: " + compoundInterest);// output message
         
     }
 }
