dab6102f559e310f47f69e7041b167f60fe74b34
TWAGIRAYESU MAHIRWE GIFT VIVIEN <149520489+GiftVivien@users.noreply.github.com>
2023-11-03 22:28:15 +0200

    Update ORoperator.java

--- src/ORoperator.java
+++ src/ORoperator.java
@@ -1,8 +1,8 @@
-public class ORoperator {
-    public static void main(String[] args) {
-        int a=10;  
-int b=5;  
-int c=20;  
+public class ORoperator {// class name 
+    public static void main(String[] args) {// mandatory main method
+        int a=10; //initialize value 
+int b=5;  //initialize value 
+int c=20;  //initialize value  
 System.out.println(a>b||a<c);//true || true = true  
 System.out.println(a>b|a<c);//true | true = true  
 //|| vs |  
