f2c447b1fe658298e4b92a86b0cdcefd367f8a08
TWAGIRAYESU MAHIRWE GIFT VIVIEN <149520489+GiftVivien@users.noreply.github.com>
2023-11-03 22:36:44 +0200

    Update compoundinterest2.java

--- src/compoundinterest2.java
+++ src/compoundinterest2.java
@@ -1,4 +1,4 @@
-public class compoundinterest2 {
+public class compoundinterest2 {// class name 
     
     double deposit;
     public compoundinterest2(double d){
@@ -6,15 +6,15 @@ public class compoundinterest2 {
         
     }
     public double Amount( double rate, int time){
-        return deposit*Math.pow(1+rate/100/12, time*12);
+        return deposit*Math.pow(1+rate/100/12, time*12);// return value
     }
     
 
-        public static void main(String[] args) {
+        public static void main(String[] args) {// mandatory main method
 
-            compoundinterest2 myInterest= new compoundinterest2(500000);
-            double newAmount=myInterest.Amount( 18, 3);
-            System.out.println("the compound interest is: " + newAmount);
+            compoundinterest2 myInterest= new compoundinterest2(500000);//object name
+            double newAmount=myInterest.Amount( 18, 3);//recalling class method
+            System.out.println("the compound interest is: " + newAmount);// output message 
 
             
         }
