6b342e765e07dbcd2d941666362708d9d4ad0d0f
TWAGIRAYESU MAHIRWE GIFT VIVIEN <149520489+GiftVivien@users.noreply.github.com>
2023-11-03 22:44:19 +0200

    Update SimpleInterest.java

--- src/SimpleInterest.java
+++ src/SimpleInterest.java
@@ -1,18 +1,18 @@
-public class SimpleInterest {
+public class SimpleInterest {// class name
     public static double Interest (double deposit,double rate, double time ){
         
-        return deposit*rate*time;
+        return deposit*rate*time;// return value
 
 
     }
     
 
-    public static void main(String[] args) {
-     double depo= 100000;
-     double percentage= 0.05;
-     double timePeriod= 5;
+    public static void main(String[] args) {// mandatory main method
+     double depo= 100000;// initialize value
+     double percentage= 0.05;// initialize value
+     double timePeriod= 5;//initialize value
      double SInterest = Interest(depo, percentage, timePeriod);
-     System.out.println("The simple interest of the deposited amount: " + SInterest);
+     System.out.println("The simple interest of the deposited amount: " + SInterest);// output message 
         
     }
 }
