77817b46dd357968e6a13c9254033a45908c9444
TWAGIRAYESU MAHIRWE GIFT VIVIEN <149520489+GiftVivien@users.noreply.github.com>
2023-11-03 22:25:47 +0200

    Update Nestedloop.java

--- src/Nestedloop.java
+++ src/Nestedloop.java
@@ -1,9 +1,9 @@
-public class Nestedloop {
-    public static void main(String[] args) {
-        for (int i = 19; i <= 20; ++i) {
+public class Nestedloop {//class name 
+    public static void main(String[] args) {// mandatory main method
+        for (int i = 19; i <= 20; ++i) {// loop for i
 
-            for (int j = 10; j < 20; ++j) {
-                System.out.println(i + "" + j);
+            for (int j = 10; j < 20; ++j) { // loop for j
+                System.out.println(i + "" + j);// output message
             }
         }
     }
