778021e37ae9eed6291ea39d3f6ee72b881a0c23
TWAGIRAYESU MAHIRWE GIFT VIVIEN <149520489+GiftVivien@users.noreply.github.com>
2023-11-03 22:26:51 +0200

    Update Nestedloop2.java

--- src/Nestedloop2.java
+++ src/Nestedloop2.java
@@ -1,8 +1,8 @@
-public class Nestedloop2 {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 5; ++i) {
-            for (int j = 1; j < 5; ++j) {
-                System.out.println(i + " " + j);
+public class Nestedloop2 {// class name 
+    public static void main(String[] args) {// mandatory main method
+        for (int i = 1; i <= 5; ++i) {//loop for i
+            for (int j = 1; j < 5; ++j) {// loop for j
+                System.out.println(i + " " + j);// output message
             }
         }
     }
