7e8a4c19f9dfd1e953b1ae5379569e37695e5a5d
TWAGIRAYESU MAHIRWE GIFT VIVIEN <149520489+GiftVivien@users.noreply.github.com>
2023-11-03 22:10:58 +0200

    Update ForLoop.java

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,8 +1,8 @@
-public class ForLoop {
+public class ForLoop {// class name 
     
-    public static void main(String[] args) {
+    public static void main(String[] args) {// mandatory main method
         for(int i=1; i<10;++i){
-            System.out.println("This game is strictly for the kids born in 200" + i);
+            System.out.println("This game is strictly for the kids born in 200" + i);//output message 
         }
     }
 }
