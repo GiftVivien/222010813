e248672a961b07c484bb349f5734c1bedbc4198e
TWAGIRAYESU MAHIRWE GIFT VIVIEN <149520489+GiftVivien@users.noreply.github.com>
2023-11-03 22:29:55 +0200

    Update ShiftOperator3.java

--- src/ShiftOperator3.java
+++ src/ShiftOperator3.java
@@ -1,10 +1,10 @@
-public class ShiftOperator3 {
-    public static void main(String[] args) {
+public class ShiftOperator3 {// class name 
+    public static void main(String[] args) {// mandatory main method
         //For positive number, >> and >>> works same  
-    System.out.println(20>>2);  
-    System.out.println(20>>>2);  
+    System.out.println(20>>2);  // output message
+    System.out.println(20>>>2);  // new line and output message 
     //For negative number, >>> changes parity bit (MSB) to 0  
-    System.out.println(-20>>2);  
-    System.out.println(-20>>>2);  
+    System.out.println(-20>>2);  // output message
+    System.out.println(-20>>>2); // new line and output message  
     }
 }
