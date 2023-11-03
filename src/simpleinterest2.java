2e30966d5f21d13bb050da004a7b840e044ee942
TWAGIRAYESU MAHIRWE GIFT VIVIEN <149520489+GiftVivien@users.noreply.github.com>
2023-11-03 22:42:04 +0200

    Update simpleinterest2.java

--- src/simpleinterest2.java
+++ src/simpleinterest2.java
@@ -1,19 +1,19 @@
- class simpleinterest2 {
+ class simpleinterest2 {// class name
     double deposit;
     public simpleinterest2(double d){
         deposit=d;
         
     }
     public double Amount( double rate, int time){
-        return deposit*rate*time;
+        return deposit*rate*time;// return value
     }
     
 
-        public static void main(String[] args) {
+        public static void main(String[] args) {// mandatory main method
 
-            simpleinterest2 myInterest= new simpleinterest2(100000);
-            double SAmount=myInterest.Amount( 0.05, 5);
-            System.out.println("the interest is: " + SAmount);
+            simpleinterest2 myInterest= new simpleinterest2(100000);// object name
+            double SAmount=myInterest.Amount( 0.05, 5);//recalling class method
+            System.out.println("the interest is: " + SAmount);// output message
 
             
         }
