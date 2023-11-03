a3d59ed68bf4f03a9e9a5a0acb88f80ef58cb50a
TWAGIRAYESU MAHIRWE GIFT VIVIEN <149520489+GiftVivien@users.noreply.github.com>
2023-11-03 22:07:35 +0200

    Update AndOperator2.java

--- src/AndOperator2.java
+++ src/AndOperator2.java
@@ -1,8 +1,8 @@
-public class AndOperator2 {
-    public static void main(String[] args) {
-        int a=10;  
-int b=5;  
-int c=20;  
+public class AndOperator2 {// class name
+    public static void main(String[] args) {// mandatory main method
+        int a=10; // initialize value 
+int b=5;// initialize value
+int c=20; // initialize value 
 System.out.println(a<b&&a++<c);//false && true = false  
 System.out.println(a);//10 because second condition is not checked  
 System.out.println(a<b&a++<c);//false && true = false  
