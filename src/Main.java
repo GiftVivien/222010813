cd3d9a272eb58d6765762551f9cd0cbfd2cbe134
TWAGIRAYESU MAHIRWE GIFT VIVIEN <149520489+GiftVivien@users.noreply.github.com>
2023-11-03 22:21:16 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        int number1 = 5;
-        int number2 = 7;
+    public static void main(String[] args) {// mandatory main method
+        int number1 = 5;// initialize value
+        int number2 = 7;// initialize value
         int result= addnumbers(number1, number2);
-        System.out.println("the sum of two numbers is " + result);
+        System.out.println("the sum of two numbers is " + result);// output message
 
         
     }
-    public static int addnumbers(int num1, int num2){
-        return num1+ num2;
+    public static int addnumbers(int num1, int num2){// class name 
+        return num1+ num2;// return value
     }
 }
