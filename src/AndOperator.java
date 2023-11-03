09b349d85fb219d1f801cf49886c8356e9c6d58e
TWAGIRAYESU MAHIRWE GIFT VIVIEN <149520489+GiftVivien@users.noreply.github.com>
2023-11-03 22:05:37 +0200

    Update AndOperator.java

--- src/AndOperator.java
+++ src/AndOperator.java
@@ -1,8 +1,8 @@
-public class AndOperator {
-    public static void main(String[] args) {
+public class AndOperator {// class name
+    public static void main(String[] args) {//mandatory main method
         int a=10;  
-int b=5;  
-int c=20;  
+int b=5;// initialize value
+int c=20;// initialize value
 System.out.println(a<b&&a<c);//false && true = false  
 System.out.println(a<b&a<c);//false & true = false  
     }
