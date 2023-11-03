8965b19e56c48704022913a9818d8ea19a94b198
TWAGIRAYESU MAHIRWE GIFT VIVIEN <149520489+GiftVivien@users.noreply.github.com>
2023-11-03 22:08:15 +0200

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,4 +1,4 @@
-public class App {
+public class App {//class name 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");// the output is going to be hello world!
     }
